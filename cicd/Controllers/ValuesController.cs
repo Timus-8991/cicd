b35d3e2798f4246a9ebca3ba0631315c101717ad
Sumit Saini <sumit.saini@nagarro.com>
2019-06-04 10:41:05 +0530

    second commit

--- cicd/Controllers/ValuesController.cs
+++ cicd/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace cicd.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Timus", "value2" };
         }
 
         // GET api/values/5
